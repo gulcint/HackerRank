5d0cb39622470776cc4bce51536c1b6348ed7458
Gülçin Taş <ce.gulcintas@gmail.com>
2019-08-11 17:52:32 +0300

    Day 8: Dictionaries and Maps

--- hackerrank10.py
+++ hackerrank10.py
@@ -7,9 +7,10 @@ the associated entry from your phone book on a new line in the form name=phoneNu
 if an entry for name is not found, print Not found instead.
 Note: Your phone book should be a Dictionary/Map/HashMap data structure.
  
- """
+"""
 
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+"""
+#this code give test case 1 error.
 
 n = int(input())
 dic = {}
@@ -25,4 +26,26 @@ for i in range(n):
     else :
         print('Not found')
       
-      
+"""
+
+# this cose is work for all cases.
+
+num = int(input())
+
+phone_book = {}
+
+for i in range(0, num):
+    entry = str(input()).split(" ")
+
+    name = entry[0]
+    phone = int(entry[1])
+    phone_book[name] = phone
+
+for j in range(0, num):
+    name = str(input())
+
+    if name in phone_book:
+        phone = phone_book[name]
+        print(name + "=" + str(phone))
+    else:
+        print("Not found")
